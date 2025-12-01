836ce3f81f31b53745b9dce7ab3d9f58f34b8f6c
Arindam Das <dasarindam.mails@gmail.com>
2025-12-01 15:05:43 +0530

    feat: add day 01 part 01 solution

---
+++ aoc-2025/src/bin/day_01.rs
@@ -0,0 +1,48 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() {
+    let input_file_path = std::env::args()
+        .nth(1)
+        .expect("Input file path not specified!");
+
+    println!("{}", input_file_path);
+
+    let file = File::open(input_file_path).expect("unable to open file");
+    let buf_reader = BufReader::new(file);
+
+    let mut dial_value: i32 = 50;
+    let mut line_count: i32 = 0;
+
+    let mut zero_count: i32 = 0;
+
+    const NUM_DIAL_ENTRIES: i32 = 100;
+
+    for line in buf_reader.lines().map_while(|x| x.ok()) {
+        line_count += 1;
+
+        let (direction, rotations) = line.split_at(1);
+
+        let rotations: i32 = rotations.parse().expect("rotations not a number!");
+
+        let delta = match direction {
+            "L" => -rotations,
+            "R" => rotations,
+            _ => break,
+        };
+
+        let new_value = dial_value + delta;
+
+        let new_value = new_value % NUM_DIAL_ENTRIES;
+
+        if new_value == 0 {
+            zero_count += 1;
+        }
+
+        dial_value = new_value;
+    }
+
+    println!("Answer: {zero_count}, read {line_count} lines.");
+}
