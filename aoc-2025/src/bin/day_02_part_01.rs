b2613ffd388b980187967544a7c55c0dc2dd4566
Arindam Das <dasarindam.mails@gmail.com>
2025-12-07 17:01:00 +0530

    feat: add day2 part1 soln

--- aoc-2025/src/bin/day_02_part_01.rs
+++ aoc-2025/src/bin/day_02_part_01.rs
@@ -1,5 +1,30 @@
 use std::{fs::File, io::Read};
 
+fn invalid_id_sum(lo: u64, hi: u64) -> u64 {
+    let half_digit_pow_10 = (f32::log10(hi as f32).floor() as u32).div_ceil(2);
+
+    let pow_10_val = 10_u64.pow(half_digit_pow_10);
+
+    let top_half = lo / pow_10_val;
+
+    let mut x = top_half;
+    let mut p = x * pow_10_val + x;
+
+    let mut sum = 0;
+
+    while p <= hi {
+        if p >= lo && f64::log10(p as f64).floor() as u64 % 2 != 0 {
+            sum += p;
+        }
+
+        x += 1;
+
+        p = x * pow_10_val + x;
+    }
+
+    sum
+}
+
 fn main() {
     let input_file_path = std::env::args()
         .nth(1)
@@ -12,7 +37,7 @@ fn main() {
     let mut buffer = [0u8; 1024];
     let mut buffer_size: usize = 0;
 
-    let mut range_vec = Vec::<(u64, u64)>::new();
+    let mut sum = 0;
 
     loop {
         let old_buffer_size = buffer_size;
@@ -46,7 +71,7 @@ fn main() {
                         let lo = lo.trim().parse().expect("not an integer!");
                         let hi = hi.trim().parse().expect("not an integer!");
 
-                        range_vec.push((lo, hi));
+                        sum += invalid_id_sum(lo, hi);
                     }
 
                     read_buffer = rem.unwrap_or(&[]);
@@ -66,5 +91,5 @@ fn main() {
         }
     }
 
-    println!("{:?}", range_vec);
+    println!("{:?}", sum);
 }
